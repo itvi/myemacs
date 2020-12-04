b459fad845158b9d89f6efa2b229170e4f7be93a
win <w@win.com>
2020-12-04 17:27:15 +0800

    my org configuration

--- custom/my-org.el
+++ custom/my-org.el
@@ -2,6 +2,9 @@
 ;;; Commentary:
 ;; http://doc.norang.ca/org-mode.html#HowToUseThisDocument
 ;;; Code:
+;;; https://hugocisneros.com/org-config/
+;;; https://www.rousette.org.uk/archives/doom-emacs-tweaks-org-journal-and-org-super-agenda/
+;;; http://www.i3s.unice.fr/~malapert/emacs_orgmode.html
 
 (use-package org-superstar
   :ensure t
@@ -11,6 +14,79 @@
   (setq org-hide-leading-stars nil)
   ;; This line is necessary.
   (setq org-superstar-leading-bullet ?\s)
+  )
+
+(use-package org-super-agenda
+  :ensure t
+  :init
+  ;; (setq org-agenda-skip-scheduled-if-done t
+  ;;       org-agenda-skip-deadline-if-done t
+  ;;       org-agenda-include-deadlines t
+  ;;       org-agenda-block-separator nil
+  ;;       org-agenda-compact-blocks t
+  ;;       org-agenda-start-day nil ;; i.e. today
+  ;;       org-agenda-span 1
+  ;;       org-agenda-start-on-weekday nil)
+  (setq org-agenda-custom-commands
+        '(("z" "Hugo view"
+           (
+            (agenda "" ((org-agenda-span 'day)
+                        (org-super-agenda-groups
+                         '((:name "Today"
+                                  :time-grid t
+                                  :date today
+                                  :todo "TODO"
+                                  :scheduled today
+                                  :order 1)))))
+            (alltodo "" ((org-agenda-overriding-header "")
+                         (org-super-agenda-groups
+                          '(;; Each group has an implicit boolean OR operator between its selectors.
+                            (:name "Passed deadline"
+                                   :and (:deadline past :todo ("TODO" "WAITING" "HOLD" "NEXT"))
+                                   :face (:background "#7f1b19")
+                                   :order 0)
+                            (:name "Due Soon"
+                                   :deadline future
+                                   :order 5)
+                            (:name "Passed scheduled"
+                                   :and (:scheduled past :todo("TODO"))
+                                   :order 2)
+                            ;; (:name "Work important"
+                            ;;        :and (:priority "A" :category "Work" :todo "TODO"))
+                            ;; (:name "Work other"
+                            ;;        :and (:category "Work" :todo "TODO"))
+                            (:name "Important"
+                                   :transformer(--> it
+                                                    (upcase it)
+                                                    (propertize it 'face '(
+                                                                           :background "orange"
+                                                                           :foreground "red")))
+                                   :priority "A")
+                            (:priority<= "B"
+                                         ;; Show this section after "Today" and "Important", because
+                                         ;; their order is unspecified, defaulting to 0. Sections
+                                         ;; are displayed lowest-number-first.
+                                         :order 7)
+                            
+                            (:name "Waiting"
+                                   :todo "WAITING"
+                                   :order 9)
+                            (:name "On hold"
+                                   :todo "HOLD"
+                                   :order 10)
+                            ;;(:discard (:not (:todo "TODO")))
+                            ))))))))
+  (setq org-super-agenda-header-map nil) ;; evil working
+  
+  ;; (custom-set-variables
+  ;;  '(org-super-agenda-header-separator 45) ;; show sth.(--) between each headers
+  ;;  )
+  
+  (custom-set-faces
+   '(org-super-agenda-header ((t (:box (:line-width 1 :color "grey") :slant italic :weight bold))))
+   )
+  :config
+  (org-super-agenda-mode)
   )
 
 (use-package org
@@ -25,13 +101,40 @@
         ("C-'" . nil)
         )
   :config
-  (add-hook 'org-agenda-mode-hook 'org-agenda-to-appt)
   (setq system-time "C")
+  (setq org-fast-tag-selection-single-key t)
+  (setq org-use-fast-todo-selection t)
+  (setq org-use-speed-commands t)
+  (setq org-src-fontify-natively t)
+  (setq org-src-tab-acts-natively t)
+  
+  (add-hook 'org-agenda-mode-hook 'org-agenda-to-appt)
   (setq org-agenda-files (list
                           "~/org"
                           "~/org/archive"
                           ))
+  ;; Compact the block agenda view
+  (setq org-agenda-compact-blocks t)
 
+  ;; ;; color file name in agenda view
+  ;; (add-hook 'org-agenda-finalize-hook
+  ;;           (lambda ()
+  ;;             (org-agenda-color-category "todo:" "#CC0000" "#EFCABE")
+  ;;             (org-agenda-color-category "project:" "#4285F4" "gray")
+  ;;             (org-agenda-color-category "refile:" "#2BBBAD" "white")
+  ;;             ))
+
+  (defun org-agenda-color-category (category backcolor forecolor)
+    "color agenda header."
+    (let ((re (rx-to-string `(seq bol (0+ space) ,category (1+ space)))))
+      (save-excursion
+        (goto-char (point-min))
+        (while (re-search-forward re nil t)
+          (add-text-properties (match-beginning 0) (match-end 0)
+                               (list 'face (list :background backcolor :foreground forecolor)))))))
+  
+
+  
   ;; TODO keyword
   (setq org-todo-keywords
         (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!/!)")
